1fa9d213c1c3a8b051ab9dc1df779b9026a3d85b
Vinicius De Paula <viniciusdepaula199@gmail.com>
2020-04-24 18:25:30 -0300

    test deploy1

--- fire-token/firebase-service.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from "firebase-admin";
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: "https://covid-zone-ab949.firebaseio.com"
-});
-export default admin;
